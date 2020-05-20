d497489e1da665b7631d58945bea3b1ea187827f
phatdroid94 <phatdroid94@gmail.com>
2020-05-20 20:50:38 +0700

    reply data for activity

--- app/src/main/java/com/example/intentexplicitdata30032020/MainActivity.java
+++ app/src/main/java/com/example/intentexplicitdata30032020/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.intentexplicitdata30032020;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -23,8 +25,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,Main2Activity.class);
                 intent.putExtra("chuoi","Hello main 2");
                 intent.putExtra("chuoi2","My names is main");
-                startActivity(intent);
+                startActivityForResult(intent,123);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 123 & resultCode == RESULT_OK && data != null){
+            Log.d("BBB",data.toString());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
