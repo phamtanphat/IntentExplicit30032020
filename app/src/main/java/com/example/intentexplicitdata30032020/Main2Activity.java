fc0fd57c7cca9bfcb4c499189247f9159cd01a67
phatdroid94 <phatdroid94@gmail.com>
2020-05-20 20:45:18 +0700

    update code

--- app/src/main/java/com/example/intentexplicitdata30032020/Main2Activity.java
+++ app/src/main/java/com/example/intentexplicitdata30032020/Main2Activity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button mBtnReplyString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        Intent intent = getIntent();
+        mBtnReplyString = findViewById(R.id.buttonIntentString);
+        final Intent intent = getIntent();
         if (intent != null){
             String chuoi1 = intent.getStringExtra("chuoi");
             String chuoi2 = intent.getStringExtra("chuoi2");
         }
+        mBtnReplyString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Main2Activity.this,MainActivity.class);
+                intent2.putExtra("main2chuoi","Thanks main");
+                setResult(RESULT_OK,intent2);
+                finish();
+            }
+        });
     }
 }
